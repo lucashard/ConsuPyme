c6b031a9e2f011f507765f29bb3d26083aee7d60
lucashard <lucas1932003@gmail.com>
2014-04-18 00:16:30 -0300

    se

---
+++ ConsupymeMedicina/Controllers/PosicionArancelariaController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Modelo;
+
+namespace ConsupymeMedicina.Controllers
+{
+    public class PosicionArancelariaController : Controller
+    {
+        private ConsuPymeDesarrolloEntities db = new ConsuPymeDesarrolloEntities();
+
+        //
+        // GET: /PosicionArancelario/
+
+        public ActionResult Index()
+        {
+            return View(db.Posicion_Arancelaria.ToList());
+        }
+
+        //
+        // GET: /PosicionArancelario/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Posicion_Arancelaria posicion_arancelaria = db.Posicion_Arancelaria.Find(id);
+            if (posicion_arancelaria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posicion_arancelaria);
+        }
+
+        //
+        // GET: /PosicionArancelario/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /PosicionArancelario/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Posicion_Arancelaria posicion_arancelaria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posicion_Arancelaria.Add(posicion_arancelaria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(posicion_arancelaria);
+        }
+
+        //
+        // GET: /PosicionArancelario/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Posicion_Arancelaria posicion_arancelaria = db.Posicion_Arancelaria.Find(id);
+            if (posicion_arancelaria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posicion_arancelaria);
+        }
+
+        //
+        // POST: /PosicionArancelario/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Posicion_Arancelaria posicion_arancelaria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(posicion_arancelaria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(posicion_arancelaria);
+        }
+
+        //
+        // GET: /PosicionArancelario/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Posicion_Arancelaria posicion_arancelaria = db.Posicion_Arancelaria.Find(id);
+            if (posicion_arancelaria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posicion_arancelaria);
+        }
+
+        //
+        // POST: /PosicionArancelario/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Posicion_Arancelaria posicion_arancelaria = db.Posicion_Arancelaria.Find(id);
+            db.Posicion_Arancelaria.Remove(posicion_arancelaria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ConsupymeMedicina/Controllers/ProductoController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Modelo;
+
+namespace ConsupymeMedicina.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private ConsuPymeDesarrolloEntities db = new ConsuPymeDesarrolloEntities();
+
+        //
+        // GET: /Producto/
+
+        public ActionResult Index()
+        {
+            var producto = db.Producto.Include(p => p.Posicion_Arancelaria);
+            var pi = producto.ToList();
+            return View(pi);
+        }
+
+        //
+        // GET: /Producto/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Posicion_ArancelariaId = new SelectList(db.Posicion_Arancelaria, "Id", "Numero_Posicion");
+            return View();
+        }
+
+        //
+        // POST: /Producto/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Producto.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Posicion_ArancelariaId = new SelectList(db.Posicion_Arancelaria, "Id", "Id", producto.Posicion_ArancelariaId);
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Posicion_ArancelariaId = new SelectList(db.Posicion_Arancelaria, "Id", "Id", producto.Posicion_ArancelariaId);
+            return View(producto);
+        }
+
+        //
+        // POST: /Producto/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Posicion_ArancelariaId = new SelectList(db.Posicion_Arancelaria, "Id", "Id", producto.Posicion_ArancelariaId);
+            return View(producto);
+        }
+
+        //
+        // GET: /Producto/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        //
+        // POST: /Producto/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Producto.Find(id);
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
